c2dda58bab222d8659cd509b800cf8c1fad46fbc
Tony Weng <tcweng91@gmail.com>
2017-06-22 12:33:40 -0700

    added Mixr icon in header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
 import LoginModalContainer from '../session/login_modal_container';
 import SignupModalContainer from '../session/signup_modal_container';
 import DemoContainer from '../session/demo_container';
 
-const Header = () => (
-  <div className='header'>
-    <div className='header-item'>
-      <LoginModalContainer />
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+  this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.props.history.push('/dashboard');
+  }
+
+  render() {
+  return (
+    <div className='header'>
+      <div onClick={this.handleClick} className='header-left'>
+        <font className="left-text" size="10">M</font>
+      </div>
+
+      <div className='header-right'>
+        <div className='header-item'>
+          <LoginModalContainer />
+        </div>
+
+        <div className='header-item'>
+          <SignupModalContainer />
+        </div>
+
+        <div className='header-item'>
+          <DemoContainer />
+        </div>
+
+      </div>
+
     </div>
+    );
+  }
+}
 
-    <div className='header-item'>
-      <SignupModalContainer />
-    </div>
-
-    <div className='header-item'>
-      <DemoContainer />
-    </div>
-
-  </div>
-);
-
-export default Header;
+export default withRouter(Header);
