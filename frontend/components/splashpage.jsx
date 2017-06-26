326c10f1ad02ab54bdd5260a4d247720836fbbd4
Tony Weng <tcweng91@gmail.com>
2017-06-25 22:20:55 -0700

    update blurb

--- frontend/components/splashpage.jsx
+++ frontend/components/splashpage.jsx
@@ -3,9 +3,15 @@ import React from 'react';
 const SplashPage = () => (
   <div>
     <section className='background-image'>
+
       <div className='logo-name'>
         Mixr
+
+        <div className='description'>
+          Your Go-To Microblogging Website
+        </div>
       </div>
+
     </section>
   </div>
 );
