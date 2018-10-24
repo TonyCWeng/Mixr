93b60cfcf6fda96c11bd9d52a5d695b732b831cb
Tony Weng <tcweng91@gmail.com>
2018-10-23 22:34:29 -0700

    delete old migrations related to paperclip

--- db/migrate/20181023220539_remove_paper_clip_columns_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemovePaperClipColumnsFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :avatar_file_name, :string
-    remove_column :users, :avatar_content_type, :string
-    remove_column :users, :avatar_file_size, :integer
-    remove_column :users, :avatar_updated_at, :datetime
-  end
-end
